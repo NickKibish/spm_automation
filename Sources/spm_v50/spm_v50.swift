48b4341607cd594acb85bdeca44c76929505acbb
Nick Kibysh <nick.kibysh@gmail.com>
2023-09-04 12:58:00 +0200

    initial commit

---
+++ Sources/spm_v50/spm_v50.swift
@@ -0,0 +1,22 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+import CoreBluetooth
+
+open class DatePrinter: NSObject {
+    open func printDate() {
+        print(Date())
+    }
+}
+
+open class MyDelegate: NSObject, CBCentralManagerDelegate {
+    open func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print(central.state)
+    }
+    
+    #if os(iOS)
+    public func centralManager(_ central: CBCentralManager, connectionEventDidOccur event: CBConnectionEvent, for peripheral: CBPeripheral) {
+        
+    }
+    #endif
+}
